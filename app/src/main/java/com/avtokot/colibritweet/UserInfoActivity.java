2c57422f764e5c0dd9104b6630c397afbfff25ab
avtokot <avtokot84@gmail.com>
2018-01-04 21:32:22 +0200

    Установил библотеку для скругления изображений и библотеку Picasso

--- app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
+++ app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
@@ -2,12 +2,23 @@ package com.avtokot.colibritweet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    private ImageView photoUserProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        photoUserProfile = (ImageView) findViewById(R.id.photo_user_profile);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .into(photoUserProfile);
     }
 }

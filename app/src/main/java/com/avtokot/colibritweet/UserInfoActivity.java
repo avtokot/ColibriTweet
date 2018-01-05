c012618e6dcd293b7abccca3470e5fafcd9222fa
avtokot <avtokot84@gmail.com>
2018-01-05 20:37:31 +0200

    В Picasso установил "заглушку" и установил "глобальный" слушатель

--- app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
+++ app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
@@ -1,9 +1,11 @@
 package com.avtokot.colibritweet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
@@ -17,8 +19,21 @@ public class UserInfoActivity extends AppCompatActivity {
 
         photoUserProfile = (ImageView) findViewById(R.id.photo_user_profile);
 
+
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
-                .into(photoUserProfile);
+                .load("http://i.imgur.com/DvpvklR.png") // откуда загружать
+                .placeholder(R.drawable.do_it) // заглушка при отсутствии интернета
+                .into(photoUserProfile, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(UserInfoActivity.this, "Фото профиля загружено", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                }); // место загрузки
+
     }
 }

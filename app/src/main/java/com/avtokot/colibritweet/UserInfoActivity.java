2c464600980e3c0e51a338254d8b680b1a69e611
avtokot <avtokot84@gmail.com>
2018-01-10 12:24:30 +0200

    Создание POJO объекта User, инициализация View объектов в коде

--- app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
+++ app/src/main/java/com/avtokot/colibritweet/UserInfoActivity.java
@@ -3,24 +3,39 @@ package com.avtokot.colibritweet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import pojo.User;
 
 public class UserInfoActivity extends AppCompatActivity {
 
-    private ImageView photoUserProfile;
+    private ImageView photoUserImageView;
+    private TextView nameTextView;
+    private TextView nickTextView;
+    private TextView descriptionTextView;
+    private TextView locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
-        photoUserProfile = (ImageView) findViewById(R.id.photo_user_profile);
+        photoUserImageView = (ImageView) findViewById(R.id.photo_user_profile);
+        nameTextView = (TextView) findViewById(R.id.user_name_text_view);
+        nickTextView = (TextView) findViewById(R.id.user_nick_text_view);
+        descriptionTextView = (TextView) findViewById(R.id.user_description_text_view);
+        locationTextView = (TextView) findViewById(R.id.user_location_text_view);
+        followersCountTextView = (TextView) findViewById(R.id.followers_count_text_view);
+        followingCountTextView = (TextView) findViewById(R.id.following_count_text_view);
 
+        loadUserInfo();
 
-        Picasso.with(this)
+       /* Picasso.with(this)
                 .load("http://i.imgur.com/DvpvklR.png") // откуда загружать
                 .placeholder(R.drawable.do_it) // заглушка при отсутствии интернета
                 .into(photoUserProfile, new Callback() {
@@ -33,7 +48,41 @@ public class UserInfoActivity extends AppCompatActivity {
                     public void onError() {
 
                     }
-                }); // место загрузки
+                }); // место загрузки*/
+    }
 
+    private void displayUserInfo(User user) {
+        Picasso.with(this)
+                .load(user.getImageUrl())
+                .placeholder(R.drawable.do_it)
+                .into(photoUserImageView);
+        nameTextView.setText(user.getName());
+        nickTextView.setText(user.getNick());
+        descriptionTextView.setText(user.getDescription());
+        locationTextView.setText(user.getLocation());
+
+        String followingCount = String.valueOf(user.getFollowingCount());
+        followingCountTextView.setText(followingCount);
+
+        String followersCount = String.valueOf(user.getFollowersCount());
+        followersCountTextView.setText(followersCount);
+    }
+
+    private void loadUserInfo() {
+        User user = getUser();
+        displayUserInfo(user);
+    }
+
+    private User getUser() {
+        return new User(
+                1L,
+                "http://i.imgur.com/DvpvklR.png",
+                "Devcolibri",
+                "@devcolibri",
+                "Sample description",
+                "Usa",
+                "23",
+                "45"
+        );
     }
 }
---
+++ app/src/main/java/pojo/User.java
@@ -0,0 +1,126 @@
+package pojo;
+
+/**
+ * Created by go on 10.01.2018.
+ */
+
+public class User {
+    private long id;
+    private String imageUrl;
+    private String name;
+    private String nick;
+    private String location;
+    private String description;
+    private String followingCount;
+    private String followersCount;
+
+    public User(long id, String imageUrl, String name, String nick, String location, String description, String followingCount, String followersCount) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.nick = nick;
+        this.location = location;
+        this.description = description;
+        this.followingCount = followingCount;
+        this.followersCount = followersCount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(String followingCount) {
+        this.followingCount = followingCount;
+    }
+
+    public String getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(String followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (imageUrl != null ? !imageUrl.equals(user.imageUrl) : user.imageUrl != null)
+            return false;
+        if (!name.equals(user.name)) return false;
+        if (!nick.equals(user.nick)) return false;
+        if (location != null ? !location.equals(user.location) : user.location != null)
+            return false;
+        if (description != null ? !description.equals(user.description) : user.description != null)
+            return false;
+        if (followingCount != null ? !followingCount.equals(user.followingCount) : user.followingCount != null)
+            return false;
+        return followersCount != null ? followersCount.equals(user.followersCount) : user.followersCount == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
+        result = 31 * result + name.hashCode();
+        result = 31 * result + nick.hashCode();
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (followingCount != null ? followingCount.hashCode() : 0);
+        result = 31 * result + (followersCount != null ? followersCount.hashCode() : 0);
+        return result;
+    }
+}
